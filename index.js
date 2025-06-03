e8c06f14bec14c3004d69bb6aaef503e1a085d14
JAlexander213 <jovan.diaz.roj@gmail.com>
2025-06-03 17:09:53 -0600

    listo

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import pkg from 'pg';
 import dotenv from 'dotenv';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('ipv4first'); // <-- ESTA LÍNEA ES LA CLAVE
 
 dotenv.config();
 
-const app = express(); // <-- ESTO FALTABA
-
+const app = express();
 const { Pool } = pkg;
 
 app.listen(process.env.PORT || 8000, () => {
@@ -15,7 +17,7 @@ app.listen(process.env.PORT || 8000, () => {
 const pool = new Pool({
   connectionString: process.env.DATABASE_URI,
   ssl: {
-    rejectUnauthorized: false 
+    rejectUnauthorized: false
   }
 });
 
