8a8e8a4f305ad84bf6b85745db0aafbc39082e70
JAlexander213 <jovan.diaz.roj@gmail.com>
2025-06-03 19:08:10 -0600

    tamaño

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+app.use(express.json({ limit: '10mb' })); // o más si necesitas
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
