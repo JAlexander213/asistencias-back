09b1a169295a3468530ddac4b16be35f7174a579
JAlexander213 <jovan.diaz.roj@gmail.com>
2025-06-03 18:08:02 -0600

    correccion 6

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import dns from 'dns';
-dns.setDefaultResultOrder('ipv4first');
-import './lib/db.js';
-import './routes/authRoutes.js';
 import cors from 'cors';
+dns.setDefaultResultOrder('ipv4first');
+
 import express from 'express';
 import pkg from 'pg';
 import dotenv from 'dotenv';
@@ -10,18 +9,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+const { Pool } = pkg;
 
-
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor backend escuchando en http://localhost:${PORT}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Servidor backend escuchando en http://localhost:${process.env.PORT || 8000}`);
 });
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));  
-
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URI,
+  ssl: {
+    rejectUnauthorized: false 
+  }
+});
 
 pool.connect((err) => {
   if (err) {
